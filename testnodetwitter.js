a85517591f862ed7bf68849d9d10f2783d7052c3
sjwhitmore <whitmore.samantha@gmail.com>
2014-09-30 15:04:28 -0400

    mongo functionality added

--- testnodetwitter.js
+++ testnodetwitter.js
@@ -1,15 +1,23 @@
 var util = require('util'),
     twitter = require('twitter');
+
+
+var databaseUrl = "tweet-collector";
+var collections = ["tweets"];
+var db = require("mongojs").connect(databaseUrl, collections);
+
+
 var twit = new twitter({
-    consumer_key: //add yours,
-    consumer_secret: //add yours,
-    access_token_key: //add yours,
-    access_token_secret: //add yours
+
+    // consumer_key: //add yours,
+    // consumer_secret: //add yours,
+    // access_token_key: //add yours,
+    // access_token_secret: //add yours
 });
 
 twit.stream('statuses/filter', {track: ['good day']}, function(stream) {
 	console.log('This is working');
     stream.on('data', function(data) {
-        console.log(util.inspect(data.text));
+        db.tweets.insert(data);
     });
 });
